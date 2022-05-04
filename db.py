ca1cdb4035c5e4e45400161854bd2fa396aa627b
Steven O'Driscoll <slodriscoll@gmail.com>
2022-05-04 11:46:18 -0600

    database startings

--- db.py
+++ db.py
@@ -1,5 +1,17 @@
 import MySQLdb
 
+import toml
+
 class DatabaseUpkeep:
     def __init__(self) -> None:
-        pass
+        # get db settings
+        data = toml.load("config.toml")
+        database = data['database']
+        self.host = database['server']
+        self.user = database['username']
+        self.password = database['password']
+        self.port = database['port']
+    
+    
+
+db = DatabaseUpkeep()
