6dfae378cc0824998fd82624321709a627fa034d
Steven O'Driscoll <slodriscoll@gmail.com>
2022-05-04 22:11:36 -0600

    more updates.

--- db.py
+++ db.py
@@ -18,10 +18,7 @@ class DatabaseUpkeep:
 
         cursor = db.cursor()
 
-        cursor.execute("SELECT VERSION();")
-
-        data = cursor.fetchone()
-        print("Database version : %s " % data)
+        
 
         db.close()
 
